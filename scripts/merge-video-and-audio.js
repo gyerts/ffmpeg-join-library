1db1ff13c4c000a71953e396088a6a76fb344e6e
gyerts <gyerts@gmail.com>
2024-01-06 01:23:37 +0200

    add silence and frozen frame if audio or video longer

--- scripts/merge-video-and-audio.js
+++ scripts/merge-video-and-audio.js
@@ -1,9 +1,39 @@
-const {FF_MPEG} = require("../constants");
+const { join, sep } = require('path');
+const {FF_MPEG, DIST_FOLDER} = require("../constants");
 const {executeCmd} = require("./execute-cmd");
+const {getDuration} = require("./get-duration");
+const {generateSilenceMp3} = require("./generate-silence-mp3");
+const {renameFile} = require("./rename-file");
+const {generateVideoFromLastFrame} = require("./generate-video-from-last-frame");
+const {logIt} = require("./log");
 
-async function mergeVideoAndAudio(videoFileName, audioFileName, mainVideoName) {
+async function mergeVideoAndAudio(videoFilePath, audioFilePath, mainVideoName) {
     console.log("Merge Video And Audio");
-    const concatCMD = `${FF_MPEG} -i ${videoFileName} -i ${audioFileName} -c:v copy -c:a aac -strict experimental ${mainVideoName}`;
+
+    const videoDuration = await getDuration(videoFilePath);
+    const audioDuration = await getDuration(audioFilePath);
+
+    if (audioDuration < videoDuration && (videoDuration - audioDuration > 0.1)) {
+        const silenceFilePath = await generateSilenceMp3(videoDuration - audioDuration);
+        const tempAudioPath = join(DIST_FOLDER, 'temp.mp3');
+        await executeCmd(
+            `${FF_MPEG} -i ${audioFilePath} -i ${silenceFilePath} -filter_complex "[0:a][1:a]concat=n=2:v=0:a=1" ${tempAudioPath}`
+        );
+        await renameFile(tempAudioPath, audioFilePath);
+        logIt(audioFilePath, 'Make audio longer', audioDuration, '->', videoDuration, 'after ->', await getDuration(audioFilePath));
+    }
+
+    if (videoDuration < audioDuration && (audioDuration - videoDuration > 0.1)) {
+        const frozenVideoPath = await generateVideoFromLastFrame(videoFilePath, audioDuration - videoDuration);
+        const tempVideoPath = join(DIST_FOLDER, 'temp.mp4');
+        await executeCmd(
+            `${FF_MPEG} -i ${videoFilePath} -i ${frozenVideoPath} -filter_complex "[0:v][1:v]concat=n=2:v=1:a=0" ${tempVideoPath}`
+        );
+        await renameFile(tempVideoPath, videoFilePath);
+        logIt(audioFilePath, 'Make video longer', videoDuration, '->', audioDuration, 'after ->', await getDuration(videoFilePath));
+    }
+
+    const concatCMD = `${FF_MPEG} -i ${videoFilePath} -i ${audioFilePath} -c:v copy -c:a aac -strict experimental ${mainVideoName}`;
     await executeCmd(concatCMD);
 }
 
